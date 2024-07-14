4452aef08f50cd88729520d0831aa2aea8a6314d
Jon2089 <abdurrehman.otaku@gmail.com>
2024-07-14 20:38:13 +0500

    app completed

--- send_email.py
+++ send_email.py
@@ -1,20 +1,15 @@
 import smtplib
 import ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "arm2001chong@gmail.com"
-password = "giqkflcqykixbnif"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "arm2001chong@gmail.com"
+    password = "giqkflcqykixbnif"
+    receiver = "abdurrehman.otaku@gmail.com"
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
 
-receiver = "abdurrehman.otaku@gmail.com"
-
-context = ssl.create_default_context()
-
-message ="""
-sup bro
-"""
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
