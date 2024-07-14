b8ba23edd6d7dece0070f14fcfc2fbd789a265b3
Jon2089 <abdurrehman.otaku@gmail.com>
2024-07-14 18:00:07 +0500

    added titles

--- app2.py
+++ app2.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
 
@@ -21,3 +22,14 @@ My contact info: joni@joker.com  phone number: 0123
 I am also a tutor. I teach maths, physics and engineering.
 """
 st.write(text)
+
+col3, col4 = st.columns(2)
+
+df = pandas.read_csv("data.csv", sep=";")
+with col3:
+    for intex, row in df[:10].iterrows():
+        st.header(row["title"])
+
+with col4:
+    for intex, row in df[10:].iterrows():
+        st.header(row["title"])
